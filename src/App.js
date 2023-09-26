fa50f95bac2e9c0801b74a7422656218fbbf19c2
Jovellmiranda <jcqmiranda.student@ua.edu.ph>
2023-09-26 18:12:31 +0800

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,51 +1,59 @@
-//File Path:
+// File Path:
 import './App.css';
-//File Path:
 
-
-function CalcuButton({label}) {
+function CalcuButton({ label }) {
   return (
-    <button>
+    <button className="CalcuButton">
       {label}
     </button>
   );
 }
-function CalcuDisplay({Display}) {
+
+function NameButton({ label }) {
+  return (
+    <button className="NameButton">
+     {label}
+    </button>
+  );
+}
+
+function CalcuDisplay({ Display }) {
   return (
     <div className='CalcuDisplay'>
       {Display}
     </div>
   );
-  }
+}
+
 function App() {
   return (
-    <div classname = 'CalcContainer'>
-      <CalcuDisplay Display = {'Hello World'}/>
-      <div classname = 'ButtonContainer'>
-        <CalcuButton label = {7}/>
-        <CalcuButton label = {8}/>
-        <CalcuButton label = {9}/>
-        <CalcuButton label = {"+"}/>
-        <CalcuButton label = {4}/>
-        <CalcuButton label = {5}/>
-        <CalcuButton label = {6}/>
-        <CalcuButton label = {"-"}/>
-        <CalcuButton label = {1}/>
-        <CalcuButton label = {2}/>
-        <CalcuButton label = {3}/>
-        <CalcuButton label = {"*"}/>
-        <CalcuButton label = {"C"}/>
-        <CalcuButton label = {0}/>
-        <CalcuButton label = {"="}/>
-        <CalcuButton label = {"÷"}/>
+    <div className='CalcContainer'>
+      <CalcuDisplay Display={'0'} />
+      <div className='ButtonContainer'>
+        <CalcuButton label={7} />
+        <CalcuButton label={8} />
+        <CalcuButton label={9} />
+        <CalcuButton label={"+"} />
+        <CalcuButton label={4} />
+        <CalcuButton label={5} />
+        <CalcuButton label={6} />
+        <CalcuButton label={"-"} />
+        <CalcuButton label={1} />
+        <CalcuButton label={2} />
+        <CalcuButton label={3} />
+        <CalcuButton label={"*"} />
+        <CalcuButton label={"C"} />
+        <CalcuButton label={0} />
+        <CalcuButton label={"="} />
+        <CalcuButton label={"÷"} />
+      </div>
+      <div className="NameButton">
+        <NameButton label={"Miranda"} />
       </div>
     </div>
   );
 }
 
 
-
-
-//Running the Code:
+// Running the Code:
 export default App;
-//Running the Code:
